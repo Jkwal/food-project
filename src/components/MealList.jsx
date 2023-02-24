dd9243754fd8c1793dfb94ba4e6d967225c87343
Yauheny Vial <mr.jk1629003@gmail.com>
2023-02-24 23:47:08 +0300

    create MVP

---
+++ src/components/MealList.jsx
@@ -0,0 +1,12 @@
+import {Meal} from './Meal'
+export function MealList({meals}) {
+    return (
+        <div className='list'>
+            {
+                meals.map(meal => (
+                    <Meal key={meal.idMeal} {...meal}/>
+                ))
+            }
+        </div>
+    )
+}
